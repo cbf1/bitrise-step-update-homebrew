518727ee659149ced57195fdd506b8198101e668
Christopher Fuller <chris@chrisfuller.me>
2019-09-10 13:26:02 -0700

    Prevent automatic cleanup

--- step.sh
+++ step.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -ex
 
+if [[ "${homebrew_cleanup}" == "yes" ]]; then
+  envman unset --key HOMEBREW_NO_INSTALL_CLEANUP
+else
+  envman add --key HOMEBREW_NO_INSTALL_CLEANUP --value 1
+fi
+
 brew update
